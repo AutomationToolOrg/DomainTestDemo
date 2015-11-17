67e1dddba11c19df2c2a9118a22a28eeb3808bdb
Nathan-Liu <ning.liu@pactera.com>
2015-11-17 11:10:29 +0800

    Update the ConfigBuilder class and add the ios-driver jar package

--- src/WebdriverEncapsulation/ConfigBuilder.java
+++ src/WebdriverEncapsulation/ConfigBuilder.java
@@ -1,12 +1,7 @@
 package WebdriverEncapsulation;
 
-import java.io.File;
 import java.util.Map;
 
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.Element;
-import org.dom4j.io.SAXReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -14,25 +9,31 @@ import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import io.selendroid.client.SelendroidDriver;
+import io.selendroid.common.SelendroidCapabilities;
+import io.selendroid.standalone.SelendroidConfiguration;
+import io.selendroid.standalone.SelendroidLauncher;
+
 public final class ConfigBuilder {
 	public static String Browser = "";
-	public static WebDriver Driver =null;
-	public static Map<String,String> driverPath=null;
+	public static WebDriver Driver = null;
+	public static Map<String, String> driverPath = null;
+
 	public static WebDriver ChooseDriver() {
 		WebDriver driver = null;
 		switch (Browser) {
 		case "IE":
-			String IEServer=driverPath.get(Browser);
+			String IEServer = driverPath.get(Browser);
 			System.setProperty("webdriver.ie.driver", IEServer);
 			// System.setProperty("webdriver.ie.driver",
-		// "Drivers/IEDriverServer_win32_2.42.0/IEDriverServer.exe");
+			// "Drivers/IEDriverServer_win32_2.42.0/IEDriverServer.exe");
 			DesiredCapabilities dc = DesiredCapabilities.internetExplorer();
 			dc.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
 			dc.setCapability("ignoreProtectedModeSettings", true);
-			 driver = new InternetExplorerDriver(dc);
-			
-			//driver = new InternetExplorerDriver();
-			 driver.manage().window().maximize();
+			driver = new InternetExplorerDriver(dc);
+
+			// driver = new InternetExplorerDriver();
+			driver.manage().window().maximize();
 			break;
 
 		case "Firefox":
@@ -42,7 +43,7 @@ public final class ConfigBuilder {
 			// profile.setPreference("network.proxy.type", 1);
 			// driver = new FirefoxDriver(profile);
 			// driver.manage().window().maximize();
-			String FFServer=driverPath.get(Browser);
+			String FFServer = driverPath.get(Browser);
 			System.setProperty("webdriver.firefox.bin", FFServer);
 			FirefoxProfile profile = new FirefoxProfile();
 			profile.setPreference("network.proxy.type", 1);
@@ -51,10 +52,23 @@ public final class ConfigBuilder {
 			break;
 
 		case "Chrome":
-			String ChromeServer=driverPath.get(Browser);
+			String ChromeServer = driverPath.get(Browser);
 			System.setProperty("webdriver.chrome.driver", ChromeServer);
 			driver = new ChromeDriver();
-			//driver.manage().window().maximize();
+			// driver.manage().window().maximize();
+		case "Android":
+			SelendroidLauncher selendroidServer = null;
+			SelendroidConfiguration config = new SelendroidConfiguration();
+			selendroidServer = new SelendroidLauncher(config);
+			selendroidServer.launchSelendroid();
+			DesiredCapabilities caps = SelendroidCapabilities.android();
+			try {
+				driver = new SelendroidDriver(caps);
+			} catch (Exception e) {
+				selendroidServer.stopSelendroid();
+				e.printStackTrace();
+			}
+			break;
 		default:
 			break;
 		}
